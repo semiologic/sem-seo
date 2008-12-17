d852d5bb2978a4afc8fa156474e88affa0d1d1c3
Denis de Bernardy <ddebarnardy@yahoo.com>
2008-12-17 11:53:51 +0000

    more post revision issues
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/sem-seo/trunk@159 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- sem-seo-admin.php
+++ sem-seo-admin.php
@@ -278,6 +278,10 @@ EOF;
 
 	function save_entry($post_ID)
 	{
+		$post = get_post($post_ID);
+		
+		if ( $post->post_type == 'revision' ) return;
+		
 		if ( current_user_can('manage_options') )
 		{
 			foreach ( array_keys(sem_seo_admin::get_fields()) as $field )
