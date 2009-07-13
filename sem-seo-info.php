7c812ee642259cfb5fdf3339c63a417ae62743f2
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-07-13 16:58:49 +0000

    Sem SEO RC2
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/sem-seo/trunk@2108 6c9c31c4-cb3d-4424-af3d-876279169c7e

---
+++ sem-seo-info.php
@@ -0,0 +1,4 @@
+<?php
+__('Semiologic SEO', 'sem-seo');
+__('All-in-one SEO plugin for WordPress', 'sem-seo');
+?>
--- sem-seo.php
+++ sem-seo.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: Semiologic SEO
 Plugin URI: http://www.semiologic.com/software/sem-seo/
-Description: All in one SEO plugin for WordPress
-Version: 2.0 RC
+Description: All-in-one SEO plugin for WordPress
+Version: 2.0 RC2
 Author: Denis de Bernardy
 Author URI: http://www.getsemiologic.com
 Text Domain: sem-seo
@@ -18,6 +18,9 @@ This software is copyright Mesoconcepts and is distributed under the terms of th
 
 http://www.mesoconcepts.com/license/
 **/
+
+
+load_plugin_textdomain('sem-seo', false, dirname(plugin_basename(__FILE__)) . '/lang');
 
 
 /**
