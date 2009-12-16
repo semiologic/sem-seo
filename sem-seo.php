f84af7621d9962d057066d70414c2e8222278db0
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-12-16 23:03:29 +0000

    bump
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/sem-seo/trunk@3751 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- sem-seo.php
+++ sem-seo.php
@@ -3,7 +3,7 @@
 Plugin Name: Semiologic SEO
 Plugin URI: http://www.semiologic.com/software/sem-seo/
 Description: All-in-one SEO plugin for WordPress
-Version: 2.0.1 alpha
+Version: 2.0.1 RC
 Author: Denis de Bernardy
 Author URI: http://www.getsemiologic.com
 Text Domain: sem-seo
